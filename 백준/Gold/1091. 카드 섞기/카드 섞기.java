19c0d6a42459f23519af6131aaff65332e4a30f7
So Hyeon <thgus345@naver.com>
2023-06-23 22:51:24 +0900

    [Gold IV] Title: 카드 섞기, Time: 208 ms, Memory: 39864 KB -BaekjoonHub

---
+++ 백준/Gold/1091. 카드 섞기/카드 섞기.java
@@ -0,0 +1,81 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N;
+	static int[] P, S;
+	static ArrayList<ArrayList<Integer>> al;
+	static int res = 0;
+	static int[] card;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		P = new int[N];
+		S = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			S[i] = Integer.parseInt(st.nextToken());
+		}
+
+		al = new ArrayList<>();
+
+		for (int i = 0; i < 3; i++) al.add(new ArrayList<>());
+
+		for (int i = 0; i < N; i++) {
+			al.get(P[i]).add(S[i]);
+		}
+
+		card = new int[N];
+		System.arraycopy(S, 0, card, 0, N);
+
+		while (!check()) {
+			if (!mix()) {
+				res = -1;
+				break;
+			}
+		}
+
+		System.out.println(res);
+
+	}
+
+	private static boolean mix() {
+		int[] temp = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			int pos = S[i];
+			int num = card[i];
+			temp[pos] = num;
+		}
+
+		System.arraycopy(temp, 0, card, 0, N);
+
+		res++;
+
+		for (int i = 0; i < N; i++) {
+			if (card[i] != S[i]) return true;
+		}
+
+		return false;
+	}
+
+	private static boolean check() {
+		for (int i = 0; i < N; i++) {
+			int num = card[i];
+			int player = i % 3;
+			if (!al.get(player).contains(num)) return false;
+		}
+		return true;
+	}
+}
