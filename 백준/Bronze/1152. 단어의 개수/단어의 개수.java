f78fdc5308cf258ef1c3d415fd5158a17c67358a
So Hyeon <thgus345@naver.com>
2023-07-01 16:35:04 +0900

    [Bronze II] Title: 단어의 개수, Time: 296 ms, Memory: 26556 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,17 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = br.readLine().split(" ");
+		int res = 0;
+		for (String str : arr) {
+			if (str.length() < 1) continue;
+			res++;
+		}
+		System.out.println(res);
+	}
+}
