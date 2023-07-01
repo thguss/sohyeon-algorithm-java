170557f220b0c8c0564f025680a50c6669cf34a1
So Hyeon <thgus345@naver.com>
2023-07-01 17:53:35 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int res = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (isPrime(num)) res++;
+		}
+		System.out.println(res);
+	}
+
+	private static boolean isPrime(int num) {
+		if (num == 0 || num == 1) return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) return false;
+		}
+		return true;
+	}
+}
