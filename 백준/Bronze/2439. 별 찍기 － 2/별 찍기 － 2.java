2a79163110a5e921f9d350aad80d4c4610573515
So Hyeon <thgus345@naver.com>
2023-07-01 16:57:01 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 15032 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < n - i; j++) {
+				sb.append(" ");
+			}
+			for (int j = 0; j < i; j++) {
+				sb.append("*");
+			}
+			System.out.println(sb);
+		}
+	}
+}
