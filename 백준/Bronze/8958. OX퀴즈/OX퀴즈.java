25edde81c5be6a9d5ca09cf3cbe6eb64bbb5cac6
So Hyeon <thgus345@naver.com>
2023-07-01 16:14:27 +0900

    [Bronze II] Title: OX퀴즈, Time: 152 ms, Memory: 14984 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int c = Integer.parseInt(br.readLine());
+
+		while (c-- > 0) {
+			String[] arr = br.readLine().split("");
+			int temp = arr[0].equals("O") ? 1 : 0;
+			int score = temp;
+			for (int i = 1; i < arr.length; i++) {
+				if (arr[i].equals("O")) {
+					temp++;
+				} else {
+					temp = 0;
+				}
+				score += temp;
+			}
+			System.out.println(score);
+		}
+	}
+}
