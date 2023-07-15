dd353bfb188c017a49d462b14fb0c67a7c8411a9
So Hyeon <thgus345@naver.com>
2023-07-15 20:52:19 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
+
+		int day = (v - b) / (a - b);
+
+		if ((v - b) % (a - b) != 0) {
+			day++;
+		}
+
+		System.out.println(day);
+	}
+}
