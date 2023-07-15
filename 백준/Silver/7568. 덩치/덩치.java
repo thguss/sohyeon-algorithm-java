dec5cc492e1032b32fdfbf59100300a6a855d233
So Hyeon <thgus345@naver.com>
2023-07-15 21:19:40 +0900

    [Silver V] Title: 덩치, Time: 148 ms, Memory: 15788 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,34 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			arr[i][0] = x;
+			arr[i][1] = y;
+		}
+
+		for (int i = 0; i < N; i++) {
+			int rank = 1;
+
+			for (int j = 0; j < N; j++) {
+				if (i == j) continue;
+
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					rank++;
+				}
+			}
+
+			System.out.print(rank + " ");
+		}
+	}
+}
