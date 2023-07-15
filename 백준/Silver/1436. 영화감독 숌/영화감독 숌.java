45e8c6d54a98a2ba6f98638e4ca21d6dbee7bb14
So Hyeon <thgus345@naver.com>
2023-07-15 19:19:23 +0900

    [Silver V] Title: 영화감독 숌, Time: 304 ms, Memory: 86512 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,24 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int num = 666;
+		int count = 1;
+
+		while (count != N) {
+			num++;
+
+			if (String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+
+		System.out.println(num);
+	}
+}
