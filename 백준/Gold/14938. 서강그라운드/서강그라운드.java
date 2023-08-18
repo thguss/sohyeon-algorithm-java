e72c581550d2fdd878888b314131c2befe5d0cf0
So Hyeon <thgus345@naver.com>
2023-08-18 18:41:17 +0900

    [Gold IV] Title: 서강그라운드, Time: 140 ms, Memory: 14536 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -1,116 +1,100 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
- 
-class Position implements Comparable<Position> {
-    int posNum; // 지역 번호
-    int weight; // 거리
- 
-    Position(int posNum, int weight) {
-        this.posNum = posNum;
-        this.weight = weight;
-    }
- 
-    @Override
-    public int compareTo(Position arg0) {
-        return weight - arg0.weight;
-    }
- 
+
+import java.io.*;
+import java.util.*;
+
+class Node implements Comparable<Node> {
+	int num;
+	int cost;
+
+	public Node(int num, int cost) {
+		this.num = num;
+		this.cost = cost;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return this.cost - o.cost;
+	}
 }
- 
+
 public class Main {
-    static int N, M, R; // 순서대로 지역의 개수, 수색 범위, 길의 개수
-    static int[] item; // 각 지역마다 들어있는 아이템의 개수
-    static ArrayList<ArrayList<Position>> a; // 인접리스트
-    static int[] dist; // 최단거리 배열
-    static boolean[] check; // 방문 확인
- 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
- 
-        item = new int[N + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            item[i] = Integer.parseInt(st.nextToken());
-        }
- 
-        a = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
-        }
- 
-        // 양방향 인접리스트 구현.
-        for (int i = 1; i <= R; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
- 
-            a.get(start).add(new Position(end, weight));
-            a.get(end).add(new Position(start, weight));
-        }
- 
-        dist = new int[N + 1];
-        check = new boolean[N + 1];
- 
-        int ans = 0;
-        for (int i = 1; i <= N; i++) {
-            ans = Math.max(ans, dijkstra(i));
-        }
- 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
- 
-    // 우선순위 큐를 활용한 다익스트라 알고리즘.
-    public static int dijkstra(int start) {
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        Arrays.fill(check, false);
- 
-        PriorityQueue<Position> pq = new PriorityQueue<>();
-        pq.offer(new Position(start, 0));
-        dist[start] = 0;
- 
-        while (!pq.isEmpty()) {
-            Position curPos = pq.poll();
-            int pos = curPos.posNum;
- 
-            if (!check[pos]) {
-                check[pos] = true;
- 
-                for (Position position : a.get(pos)) {
-                    if (!check[position.posNum] && dist[position.posNum] > dist[pos] + position.weight) {
-                        dist[position.posNum] = dist[pos] + position.weight;
-                        pq.add(new Position(position.posNum, dist[position.posNum]));
-                    }
-                }
-            }
-        }
- 
-        int res = 0;
-        
-        // 수색 범위 내로 갈 수 있는 모든 지역의 아이템 개수를 더함.
-        for (int i = 1; i <= N; i++) {
-            if (dist[i] <= M) {
-                res += item[i];
-            }
-        }
- 
-        return res;
-    }
- 
+
+	static int n, m;
+	static ArrayList<ArrayList<Node>> graph;
+	static int[] item;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken()); // 지역의 개수
+		m = Integer.parseInt(st.nextToken()); // 수색 범위
+		int r = Integer.parseInt(st.nextToken()); // 길의 개수
+
+		item = new int[n + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			item[i] = Integer.parseInt(st.nextToken());
+		}
+
+		graph = new ArrayList<>();
+		for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+
+		for (int i = 0; i < r; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int l = Integer.parseInt(st.nextToken());
+			graph.get(a).add(new Node(b, l));
+			graph.get(b).add(new Node(a, l));
+		}
+
+		int max = 0;
+
+		// System.out.println(Arrays.toString(item));
+
+		for (int i = 1; i <= n; i++) {
+			max = Math.max(max, dijkstra(i));
+		}
+
+		System.out.println(max);
+
+	}
+
+	private static int dijkstra(int start) {
+		int[] dist = new int[n + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[start] = 0;
+
+		boolean[] visited = new boolean[n + 1];
+
+		Queue<Node> queue = new PriorityQueue<>();
+		queue.add(new Node(start, 0));
+
+		while(!queue.isEmpty()) {
+			Node cur = queue.poll();
+			int num = cur.num;
+
+			if (visited[num]) continue;
+			visited[num] = true;
+
+			for (Node next : graph.get(num)) {
+				if (!visited[next.num] && dist[next.num] > dist[num] + next.cost) {
+					dist[next.num] = dist[num] + next.cost;
+					queue.add(new Node(next.num, dist[next.num]));
+				}
+			}
+		}
+
+		int cost = 0;
+		for (int i = 1; i <= n; i++) {
+			if (dist[i] <= m) {
+				cost += item[i];
+			}
+		}
+
+		return cost;
+	}
+
 }
