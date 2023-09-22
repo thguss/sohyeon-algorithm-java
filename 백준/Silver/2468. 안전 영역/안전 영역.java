af7b3bb524b0cc76fca2a9daccf077b709fc6e18
Sohyeon Kim <thgus345@naver.com>
2023-09-22 15:54:37 +0900

    [Silver I] Title: 안전 영역, Time: 316 ms, Memory: 55420 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,79 +1,67 @@
-
-
 import java.util.*;
 import java.io.*;
 
-class Position2 {
-	int x;
-	int y;
-
-	Position2(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
 public class Main {
 
-	static int N;
-	static int[][] board;
-	static int MAX_HEIGHT = Integer.MIN_VALUE;
-	static int max = Integer.MIN_VALUE;
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, 1, -1};
+	static int[] dx = {0, 0, -1, 1};
+	static int[] dy = {1, -1, 0, 0};
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+
+		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
 		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
-				MAX_HEIGHT = Math.max(MAX_HEIGHT, board[i][j]);
+				map[i][j] = Integer.parseInt(st.nextToken());
+				min = Math.min(map[i][j], min);
+				max = Math.max(map[i][j], max);
 			}
 		}
 
-		for (int h = 0; h <= MAX_HEIGHT; h++) {
-			max = Math.max(max, getSafeZone(h));
-		}
+		int res = Integer.MIN_VALUE;
 
-		System.out.println(max);
-	}
-
-	private static int getSafeZone(int h) {
-		int count = 0;
-		boolean[][] visited = new boolean[N][N];
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (board[i][j] <= h || visited[i][j]) continue;
-				bfs(h, visited, i, j);
-				count++;
+		for (int h = min - 1; h <= max; h++) {
+			int cnt = 0;
+			boolean[][] visited = new boolean[N][N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][j] > h && !visited[i][j]) {
+						// 물에 안 잠기는 곳, 아직 방문 안 한 곳
+						bfs(map, N, h, i, j, visited);
+						cnt++;
+					}
+				}
 			}
+			res = Math.max(res, cnt);
 		}
 
-		return count;
+		System.out.println(res);
+
 	}
 
-	private static void bfs(int h, boolean[][] visited, int x, int y) {
-		Queue<Position2> queue = new LinkedList<>();
-		queue.offer(new Position2(x, y));
-		visited[x][y] = true;
+	private static void bfs(int[][] map, int N, int h, int i, int j, boolean[][] visited) {
+		visited[i][j] = true;
+
+		Queue<int[]> queue = new LinkedList<>();
+		queue.offer(new int[] {i, j});
 
 		while (!queue.isEmpty()) {
-			Position2 cur = queue.poll();
-
-			for (int i = 0; i < 4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				if (0 <= nx && nx < N && 0 <= ny && ny < N) {
-					if (visited[nx][ny] || board[nx][ny] <= h) continue;
-					queue.offer(new Position2(nx, ny));
+			int[] cur = queue.poll();
+			for (int d = 0; d < 4; d++) {
+				int nx = cur[0] + dx[d], ny = cur[1] + dy[d];
+				if (0 <= nx && nx < N && 0 <= ny && ny < N && !visited[nx][ny] && map[nx][ny] > h) {
+					// 범위 내, 방문 안 한 곳, 물에 안 잠기는 곳
 					visited[nx][ny] = true;
+					queue.offer(new int[] {nx, ny});
 				}
 			}
 		}
 	}
+
 }
