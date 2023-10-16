26135bdc347b692c948c14bcf56ebc9f24c5e6aa
Sohyeon Kim <thgus345@naver.com>
2023-10-16 16:15:54 +0900

    [Gold III] Title: 파티, Time: 644 ms, Memory: 87068 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,91 +1,58 @@
-
-
 import java.util.*;
 import java.io.*;
 
-class City implements Comparable<City> {
-	int n;
-	int c;
-
-	City(int num, int cost) {
-		this.n = num;
-		this.c = cost;
-	}
-
-	@Override
-	public int compareTo(City o) {
-		return this.c - o.c;
-	}
-
-	@Override
-	public String toString() {
-		return "City{" +
-			"n=" + n +
-			", c=" + c +
-			'}';
-	}
-}
-
 public class Main {
-
-	static int N, M, X;
-	static ArrayList<ArrayList<int[]>> graph;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		X = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
 
-		graph = new ArrayList<>();
-		for (int i = 0; i <= N; i++) graph.add(new ArrayList<>());
+		List<List<int[]>> al = new ArrayList<>();
+		for (int i = 0; i <= N; i++) al.add(new ArrayList<>());
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			graph.get(a).add(new int[] {b, c});
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			al.get(s).add(new int[] {e, t});
+		}
+
+		int[][] dist = new int[N + 1][N + 1];
+		for (int[] arr : dist) Arrays.fill(arr, Integer.MAX_VALUE);
+		for (int i = 0; i <= N; i++) dist[i][i] = 0;
+
+		for (int i = 1; i <= N; i++) {
+			dijkstra(al, dist[i], i);
 		}
 
 		int max = Integer.MIN_VALUE;
-
+		
 		for (int i = 1; i <= N; i++) {
-			if (i == X) continue;
-			max = Math.max(max, dijkstra(i, X) + dijkstra(X, i));
+			int cost = dist[i][X] + dist[X][i];
+			max = Math.max(max, cost);
 		}
 
 		System.out.println(max);
 
 	}
 
-	private static int dijkstra(int from, int to) {
-		PriorityQueue<City> queue = new PriorityQueue<>();
-
-		boolean[] visited = new boolean[N + 1];
-		int[] dist = new int[N + 1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[from] = 0;
-
-		queue.offer(new City(from, 0));
+	private static void dijkstra(List<List<int[]>> al, int[] dist, int x) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(x);
 
 		while (!queue.isEmpty()) {
-			City cur = queue.poll();
-
-			if (visited[cur.n]) continue;
-			visited[cur.n] = true;
-
-			for (int[] arr : graph.get(cur.n)) {
-				if (visited[arr[0]]) continue;
-				if (dist[arr[0]] > dist[cur.n] + arr[1]) {
-					dist[arr[0]] = dist[cur.n] + arr[1];
-					queue.offer(new City(arr[0], dist[arr[0]]));
+			int now = queue.poll();
+			for (int[] next : al.get(now)) {
+				if (dist[next[0]] > dist[now] + next[1]) {
+					dist[next[0]] = dist[now] + next[1];
+					queue.add(next[0]);
 				}
 			}
 		}
-
-		return dist[to];
 	}
 }
