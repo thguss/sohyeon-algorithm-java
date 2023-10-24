1d30fad937e02348e464858fc0091f2cd740eb95
Sohyeon Kim <thgus345@naver.com>
2023-10-24 23:37:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 224 ms, Memory: 53916 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,9 +1,8 @@
-
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,27 +10,28 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 
-		int[][] dp = new int[n+1][k+1];
-		int[][] dummy = new int[n][2];
+		int[][] arr = new int[n][2];
 
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			dummy[i] = new int[] {w, v};
+			int w = Integer.parseInt(st.nextToken()); // 무게
+			int v = Integer.parseInt(st.nextToken()); // 가치
+			arr[i] = new int[] {w, v};
 		}
 
+		int[][] dp = new int[n + 1][k + 1];
 		for (int j = 0; j <= k; j++) {
 			for (int i = 1; i <= n; i++) {
-				if (j < dummy[i-1][0]) {
-					dp[i][j] = dp[i-1][j];
+				if (j < arr[i - 1][0]) { // 못 담는다.
+					dp[i][j] = dp[i - 1][j];
 				} else {
-					int pre = j - dummy[i-1][0];
-					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][pre] + dummy[i-1][1]);
+					int pre = j - arr[i - 1][0];
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][pre] + arr[i - 1][1]);
 				}
 			}
 		}
 
 		System.out.println(dp[n][k]);
+
 	}
 }
