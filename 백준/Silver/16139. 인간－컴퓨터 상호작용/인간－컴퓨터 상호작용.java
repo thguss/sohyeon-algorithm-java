2dc4cf31236bf26ce5a49052f66ee63b2480522b
Sohyeon Kim <thgus345@naver.com>
2023-11-10 22:49:32 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 808 ms, Memory: 110988 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -10,22 +10,28 @@ public class Main {
 		String str = br.readLine();
 		int T = Integer.parseInt(br.readLine());
 
+		int[][] arr = new int[str.length() + 1]['z' - 'a' + 1]; // 누적합 배열
+
+		// 누적합 배열 초기화
+		arr[1][str.charAt(0) - 'a']++; // 처음 값
+		for (int i = 2; i <= str.length(); i++) {
+			int idx = str.charAt(i - 1) - 'a'; // 탐색 중인 문자
+			for (int j = 0; j < 'z' - 'a' + 1; j++) {
+				int before = arr[i - 1][j]; // 이전 누적합
+				arr[i][j] = (j == idx) ? before + 1 : before;
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+
 		while (T-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			char c = st.nextToken().charAt(0);
-			int l = Integer.parseInt(st.nextToken());
-			int r = Integer.parseInt(st.nextToken());
-			System.out.println(count(str, c, l, r));
+			int l = Integer.parseInt(st.nextToken()) + 1;
+			int r = Integer.parseInt(st.nextToken()) + 1;
+			sb.append(arr[r][c - 'a'] - arr[l - 1][c - 'a']).append("\n");
 		}
-	}
 
-	private static int count(String str, char c, int l, int r) {
-		int cnt = 0;
-		for (int i = l; i <= r; i++) {
-			if (str.charAt(i) == c) {
-				cnt++;
-			}
-		}
-		return cnt;
+		System.out.println(sb);
 	}
 }
