8cd5ee4e9f15f4086789e6865f37fc487e7d9355
Sohyeon Kim <thgus345@naver.com>
2023-11-21 20:54:43 +0900

    [level 1] Title: 폰켓몬, Time: 5.71 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,14 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            if (!map.containsKey(num)) {
-                map.put(num, 0);
-            }
-            map.put(num, map.get(num) + 1);
-        }
-        System.out.println(map.size());
         
-        return map.size() >= nums.length / 2 ? nums.length / 2 : map.size();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        return map.size() >= (nums.length / 2) ? (nums.length / 2) : map.size();
     }
 }
