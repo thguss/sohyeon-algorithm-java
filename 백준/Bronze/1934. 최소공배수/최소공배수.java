ea8cc497af4146124f31c1375cec33760d6ea88f
Sohyeon Kim <thgus345@naver.com>
2024-02-12 19:34:07 +0900

    [Bronze I] Title: 최소공배수, Time: 192 ms, Memory: 14988 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,62 @@
+import java.lang.reflect.Member;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
+import java.io.*;
+
+import org.w3c.dom.Node;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int T = Integer.parseInt(br.readLine());
+
+		while (T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			long A = Long.parseLong(st.nextToken());
+			long B = Long.parseLong(st.nextToken());
+
+			System.out.println(getLCM(A, B));
+		}
+
+	}
+
+	private static long getLCM(long num1, long num2) {
+		if (num2 > num1) {
+			long temp = num1;
+			num1 = num2;
+			num2 = temp;
+		}
+		long gcd = getGCD(num1, num2);
+		return getLCM((num1 * num2) / gcd, num1, num2);
+	}
+
+	private static long getLCM(long lcm, long num1, long num2) {
+		long gcd = getGCD(lcm, num1);
+		lcm = (lcm * num1) / gcd;
+
+		gcd = getGCD(lcm, num2);
+		lcm = (lcm * num2) / gcd;
+
+		return lcm;
+	}
+
+	private static long getGCD(long num1, long num2) {
+		if (num2 > num1) {
+			long temp = num1;
+			num1 = num2;
+			num2 = temp;
+		}
+
+		if (num1 % num2 == 0) {
+			return num2;
+		}
+
+		return getGCD(num2, num1 % num2);
+	}
+}
