b4a5dc701ea71c4aa441ddc7a869c0399d5b7ddc
Sohyeon Kim <thgus345@naver.com>
2024-03-02 20:58:40 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int A, B, C;
+
+	public static void main(String[] args) throws Exception {
+		input();
+
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C) * (B%C))%C);
+	}
+
+	static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+	}
+
+}
