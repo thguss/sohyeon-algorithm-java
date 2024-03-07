a1f5e2aee40b77002512369265e61a1c4845a765
Sohyeon Kim <thgus345@naver.com>
2024-03-07 19:50:46 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -5,7 +5,25 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		int N = getN();
-		System.out.println(greedy(N));
+		System.out.println(dp(N));
+	}
+
+	static int dp(int N) {
+		int[] dp = new int[N + 1];
+		if (N >= 3) dp[3] = 1;
+		if (N >= 5) dp[5] = 1;
+
+		for (int i = 6; i <= N; i++) {
+			if (i % 5 == 0) {
+				dp[i] = dp[i - 5] + 1;
+			} else if (i % 3 == 0) {
+				dp[i] = dp[i - 3] + 1;
+			} else if (dp[i - 5] != 0 && dp[i - 3] != 0) {
+				dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
+			}
+		}
+
+		return dp[N] != 0 ? dp[N] : -1;
 	}
 
 	static int greedy(int N) {
