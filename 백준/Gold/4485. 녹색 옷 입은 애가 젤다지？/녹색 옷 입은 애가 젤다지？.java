29e1e7547cb83d06f03618358f70c2b5d1bdc52f
Sohyeon Kim <thgus345@naver.com>
2024-03-22 15:07:20 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 224 ms, Memory: 20552 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,17 +1,16 @@
 import java.util.*;
 import java.io.*;
 
-import org.w3c.dom.Node;
-
 public class Main {
+
 	static class Position implements Comparable<Position> {
-		int r;
-		int c;
+		int x;
+		int y;
 		int cost;
 
-		Position(int r, int c, int cost) {
-			this.r = r;
-			this.c = c;
+		Position(int x, int y, int cost) {
+			this.x = x;
+			this.y = y;
 			this.cost = cost;
 		}
 
@@ -21,61 +20,75 @@ public class Main {
 		}
 	}
 
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, 1, -1};
+
 	public static void main(String[] args) throws Exception {
+		System.out.println(input());
+	}
+
+	static String input() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int num = 1;
+		StringBuilder sb = new StringBuilder();
 
 		while (true) {
-			int N = Integer.parseInt(br.readLine()); // 동굴의 크기
+			int N = Integer.parseInt(br.readLine());
 
-			if (N == 0)
-				break;
+			if (N == 0) break;
 
-			int[][] map = new int[N][N];
+			int[][] board = new int[N][N];
 
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+					board[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 
-			int res = bfs(map, N);
-			System.out.println("Problem " + num + ": " + res);
+			solve(sb, num, N, board);
+			sb.append("\n");
 			num++;
 		}
+
+		return sb.toString();
 	}
 
-	private static int bfs(int[][] map, int N) {
-		Queue<Position> queue = new PriorityQueue<>();
-		queue.add(new Position(0, 0, map[0][0]));
+	static void solve(StringBuilder sb, int i, int N, int[][] board) {
+		sb.append("Problem ").append(i).append(": ").append(bfs(N, board));
+	}
 
+	static int bfs(int N, int[][] board) {
 		int[][] move = new int[N][N];
-		for (int[] arr : move)
-			Arrays.fill(arr, Integer.MAX_VALUE);
-		move[0][0] = map[0][0];
+		for (int[] arr : move) Arrays.fill(arr, Integer.MAX_VALUE);
+		move[0][0] = board[0][0];
 
-		int[] dr = {0, 0, 1, -1};
-		int[] dc = {1, -1, 0, 0};
+		Queue<Position> queue = new PriorityQueue<>();
+		queue.add(new Position(0, 0, board[0][0]));
 
 		while (!queue.isEmpty()) {
 			Position cur = queue.poll();
 
-			if (cur.r == N - 1 && cur.c == N - 1)
+			if (cur.x == N - 1 && cur.y == N - 1) {
 				return cur.cost;
+			}
 
 			for (int i = 0; i < 4; i++) {
-				int nr = cur.r + dr[i];
-				int nc = cur.c + dc[i];
-				if (0 <= nr && nr < N && 0 <= nc && nc < N && move[nr][nc] > cur.cost + map[nr][nc]) {
-					move[nr][nc] = cur.cost + map[nr][nc];
-					queue.add(new Position(nr, nc, move[nr][nc]));
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+
+				if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+					if (move[nx][ny] > cur.cost + board[nx][ny]) {
+						move[nx][ny] = board[nx][ny] + cur.cost;
+						queue.add(new Position(nx, ny, move[nx][ny]));
+					}
 				}
 			}
 		}
 
 		return -1;
 	}
+
 }
