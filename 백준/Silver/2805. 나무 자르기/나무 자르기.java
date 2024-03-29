3ccbab9bddc6b025712046d5c3831c50b260c45e
Sohyeon Kim <thgus345@naver.com>
2024-03-29 17:10:31 +0900

    [Silver II] Title: 나무 자르기, Time: 552 ms, Memory: 123236 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,63 +1,50 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static int res = 0;
-	
-	public static void main(String args[]) throws IOException {
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		long m = Long.parseLong(st.nextToken());
-		
+
+		int N = Integer.parseInt(st.nextToken()); // 나무 개수
+		long M = Long.parseLong(st.nextToken()); // 가져가려고 하는 길이
+		long[] arr = new long[N];
+		long max = Integer.MIN_VALUE;
+
 		st = new StringTokenizer(br.readLine());
-		int arr[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Long.parseLong(st.nextToken());
+			max = Math.max(max, arr[i]);
 		}
-		
-		Arrays.sort(arr);
-		
-		getTree(n, m, arr);
-		
-		System.out.println(res);
-		
-	}
-	
-	static void getTree(int n, long m, int[] arr) {
-		int start = 0;
-		int end = arr[n - 1];
-		
-		while (start <= end) {
-			int mid = (start + end) / 2;
-			long treeNum = getTreeNum(n, m, arr, mid);
-			
-			if (m == treeNum) {
-				res = mid;
-				return;
-			}
-			else if (treeNum > m) {
-				res = mid;
-				start = mid + 1;
-			}
-			else { // treeNum < m
-				end = mid - 1;
+
+		long left = 0;
+		long right = max;
+		long heightMax = Long.MIN_VALUE;
+
+		while (left <= right) {
+			long mid = (left + right) / 2;
+
+			if (cutting(arr, mid, M)) {
+				heightMax = Math.max(heightMax, mid);
+				left = mid + 1;
+			} else {
+				right = mid - 1;
 			}
 		}
-	}
-	
-	static long getTreeNum(int n, long m, int[] arr, int height) {
-		long cnt = 0;
-		
-		for (int i = 0; i < n; i++) {
-			if (cnt > m) return cnt;
-			if (arr[i] > height) cnt += (arr[i] - height);
-		}
-		
-		return cnt;
+
+		System.out.println(heightMax);
+
 	}
 
+	private static boolean cutting(long[] arr, long height, long M) {
+		long sum = 0;
+
+		for (long num : arr) {
+			if (num < height) continue;
+			sum += num - height;
+		}
+
+		return sum >= M;
+	}
 }
