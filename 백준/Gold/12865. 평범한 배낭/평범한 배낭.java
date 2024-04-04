f857aa89fa07c932f2ca8b19af8e45705f93890f
Sohyeon Kim <thgus345@naver.com>
2024-04-05 00:31:00 +0900

    [Gold V] Title: 평범한 배낭, Time: 224 ms, Memory: 53816 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -2,36 +2,45 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	static int N, K;
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[][] arr = getArr();
+		System.out.println(dp(arr));
+	}
 
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
+	static int dp(int[][] arr) {
+		int[][] dp = new int[N + 1][K + 1];
 
-		int[][] arr = new int[n][2];
-
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken()); // 무게
-			int v = Integer.parseInt(st.nextToken()); // 가치
-			arr[i] = new int[] {w, v};
-		}
-
-		int[][] dp = new int[n + 1][k + 1];
-		for (int j = 0; j <= k; j++) {
-			for (int i = 1; i <= n; i++) {
-				if (j < arr[i - 1][0]) { // 못 담는다.
-					dp[i][j] = dp[i - 1][j];
-				} else {
-					int pre = j - arr[i - 1][0];
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][pre] + arr[i - 1][1]);
+		for (int j = 1; j <= K; j++) { // 무게
+			for (int i = 1; i <= N; i++) { // 물품 수
+				dp[i][j] = dp[i - 1][j];
+				if (j >= arr[i][0]) { // 물품 담을 수 있는 지 확인
+					int remain = j - arr[i][0]; // 더 채울 수 있는 무게
+					dp[i][j] = Math.max(dp[i][j], dp[i - 1][remain] + arr[i][1]); // 더 채울 수 있는 무게 중 최대 가치 + 현 가치
 				}
 			}
 		}
 
-		System.out.println(dp[n][k]);
-
+		return dp[N][K];
 	}
+
+	static int[][] getArr() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken()); // 물품의 수
+		K = Integer.parseInt(st.nextToken()); // 버틸 수 있는 무게
+
+		int[][] arr = new int[N + 1][2];
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken()); // 무게
+			arr[i][1] = Integer.parseInt(st.nextToken()); // 가치
+		}
+
+		return arr;
+	}
+
 }
