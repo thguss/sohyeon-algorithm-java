7e0791aa73475f7f51cd9bb1cc74205ca7afdd06
Sohyeon Kim <thgus345@naver.com>
2024-04-12 01:23:01 +0900

    [Gold V] Title: 치킨 배달, Time: 208 ms, Memory: 15008 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,63 +2,67 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static boolean[] open;
-	static int answer = Integer.MAX_VALUE;
+
+	static int N, M;
+	static List<int[]> al = new ArrayList<>();
+	static List<int[]> homes = new ArrayList<>();
+	static int min = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws Exception {
+		int[][] board = getBoards();
+		comb(new int[M][2], 0, 0, board);
+		System.out.println(min);
+	}
+
+	static void comb(int[][] arr, int start, int index, int[][] board) {
+		if (index == M) {
+			min = Math.min(min, getDistance(arr));
+			return;
+		}
+
+		for (int i = start; i < al.size(); i++) {
+			arr[index][0] = al.get(i)[0];
+			arr[index][1] = al.get(i)[1];
+			comb(arr, i + 1, index + 1, board);
+		}
+	}
+
+	static int getDistance(int[][] arr) {
+		int sum = 0;
+
+		for (int[] home : homes) {
+			int minDistance = Integer.MAX_VALUE;
+			for (int[] open : arr) {
+				minDistance = Math.min(minDistance, Math.abs(home[0] - open[0]) + Math.abs(home[1] - open[1]));
+			}
+			sum += minDistance;
+		}
+
+		return sum;
+	}
+
+	static int[][] getBoards() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
 		int[][] arr = new int[N][N];
-		List<int[]> houseList = new ArrayList<>();
-		List<int[]> chickenList = new ArrayList<>();
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
-				if (arr[i][j] == 1) {
-					houseList.add(new int[] {i, j});
-				} else if (arr[i][j] == 2) {
-					chickenList.add(new int[] {i, j});
+				if (arr[i][j] == 2) {
+					al.add(new int[] {i, j});
+				} else if (arr[i][j] == 1) {
+					homes.add(new int[] {i, j});
 				}
 			}
 		}
 
-		open = new boolean[chickenList.size()];
-		dfs(0, 0, M, houseList, chickenList);
-
-		System.out.println(answer);
+		return arr;
 	}
 
-	private static void dfs(int start, int cnt, int M, List<int[]> houseList, List<int[]> chickenList) {
-		if (cnt == M) {
-			int res = 0;
-
-			for (int i = 0; i < houseList.size(); i++) {
-				int temp = Integer.MAX_VALUE;
-				for (int j = 0; j < chickenList.size(); j++) {
-					if (open[j]) {
-						int[] house = houseList.get(i);
-						int[] chicken = chickenList.get(j);
-						int distance = Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]);
-						temp = Math.min(temp, distance);
-					}
-				}
-				res += temp;
-			}
-
-			answer = Math.min(answer, res);
-			return;
-		}
-
-		for (int i = start; i < chickenList.size(); i++) {
-			open[i] = true;
-			dfs(i + 1, cnt + 1, M, houseList, chickenList);
-			open[i] = false;
-		}
-	}
 }
