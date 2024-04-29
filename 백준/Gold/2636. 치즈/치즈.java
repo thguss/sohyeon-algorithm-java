4ef0a175720c224be0716444190917c6ee75a3f1
Sohyeon Kim <thgus345@naver.com>
2024-04-29 21:06:05 +0900

    [Gold IV] Title: 치즈, Time: 160 ms, Memory: 16088 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,114 +1,88 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-class Position {
-	int x;
-	int y;
-
-	public Position(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
 	static int N, M;
-	static int[][] cheese;
 	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, 1, -1};
-	static int time, count;
+	static int cnt = 0;
 
 	public static void main(String[] args) throws Exception {
-		getInput();
+		int[][] board = getBoard();
 
-		time = 0;
-		count = 0;
+		int time = 0;
 
-		while (!isEnd()) {
-			bfs();
+		while (isExistCheese(board)) {
+			melt(board);
+			time++;
 		}
 
 		System.out.println(time);
-		System.out.println(count);
+		System.out.println(cnt);
 	}
 
-	private static void bfs() {
-		boolean[][] visited = new boolean[N][M];
+	static void melt(int[][] board) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {0, 0});
 
-		Queue<Position> queue = new LinkedList<>();
-		queue.add(new Position(0, 0));
+		boolean[][] visited = new boolean[N][M];
 		visited[0][0] = true;
 
 		while (!queue.isEmpty()) {
-			Position position = queue.remove();
+			int[] cur = queue.poll();
+
+			if (board[cur[0]][cur[1]] == 1) {
+				board[cur[0]][cur[1]] = 0;
+				continue;
+			}
 
 			for (int i = 0; i < 4; i++) {
-				int nx = position.x + dx[i];
-				int ny = position.y + dy[i];
+				int nx = cur[0] + dx[i];
+				int ny = cur[1] + dy[i];
 
-				if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-					if (visited[nx][ny]) continue;
-
-					if (cheese[nx][ny] == 1) {
-						cheese[nx][ny] = 2;
-					} else if (cheese[nx][ny] == 0) {
-						queue.add(new Position(nx, ny));
-					}
-
+				if (0 <= nx && nx < N && 0 <= ny && ny < M && !visited[nx][ny]) {
 					visited[nx][ny] = true;
+					queue.add(new int[] {nx, ny});
 				}
 			}
 		}
-
-		time++;
-		count = getCount();
 	}
 
-	private static boolean isEnd() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (cheese[i][j] == 1) return false;
-			}
-		}
-		return true;
-	}
-
-	private static int getCount() {
-		int result = 0;
+	static boolean isExistCheese(int[][] board) {
+		int cheeseCount = 0;
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (cheese[i][j] == 2) {
-					result++;
-					cheese[i][j] = 0;
-				}
+				if (board[i][j] == 1) cheeseCount++;
 			}
 		}
 
-		return result;
+		if (cheeseCount > 0) {
+			cnt = cheeseCount;
+			return true;
+		}
+
+		return false;
 	}
 
-	private static void getInput() throws IOException {
+	static int[][] getBoard() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		cheese = new int[N][M];
+		int[][] board = new int[N][M];
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				cheese[i][j] = Integer.parseInt(st.nextToken());
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+
+		return board;
 	}
+
 }
