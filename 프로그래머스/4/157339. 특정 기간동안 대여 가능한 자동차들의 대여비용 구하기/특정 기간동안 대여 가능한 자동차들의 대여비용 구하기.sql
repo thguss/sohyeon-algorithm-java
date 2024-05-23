59389605b5ab68921b842e8c092ad97f4e7be4c9
Sohyeon Kim <thgus345@naver.com>
2024-05-23 16:25:39 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,4 +1,16 @@
--- 코드를 입력하세요
+# select distinct(A.car_id), A.car_type, ((A.daily_fee * 30) * (100 - C.discount_rate) / 100) as fee
+# from car_rental_company_car A
+#     inner join car_rental_company_rental_history B on A.car_id = B.car_id
+#     inner join car_rental_company_discount_plan C on A.car_type = C.car_type
+# where (A.car_type like '세단' or A.car_type = 'SUV')
+#     and not (B.start_date like '2022-11%' 
+#              or B.end_date like '2022-11%' 
+#              or (B.start_date < '2022-11-01' and B.end_date > '2022-11-30')
+#             )
+#     and C.duration_type like '30일 이상'
+#     and ((A.daily_fee * 30) * (100 - C.discount_rate) / 100) between 500000 and 2000000
+# order by fee desc, A.car_type, A.car_id desc
+
 SELECT DISTINCT(A.CAR_ID), A.CAR_TYPE, ROUND(A.DAILY_FEE * 30 * (100 - C.DISCOUNT_RATE) / 100) AS FEE
 FROM CAR_RENTAL_COMPANY_CAR A
 JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B ON A.CAR_ID = B.CAR_ID
