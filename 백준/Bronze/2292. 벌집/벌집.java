fa874dec722b60af27bd08239965f2cfaf2387ec
Sohyeon Kim <thgus345@naver.com>
2024-09-27 13:18:55 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,19 +1,47 @@
-
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    public static void main(String[] args) throws Exception {
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int range = 2;
 
-		int room = 1;
-		int circular = 1;
-		while (room < N) {
-			room += 6 * circular;
-			circular++;
-		}
+        if (N == 1) bw.write(String.valueOf(1));
+        else {
+            while (range <= N) {
+                range += (6 * count);
+                count++;
+            }
+            bw.write(String.valueOf(count));
+        }
+        
+        bw.flush();
+    }
 
-		System.out.println(circular);
-	}
+    private static String getType(int[] arr) {
+        Arrays.sort(arr);
+        int a = arr[0];
+        int b = arr[1];
+        int c = arr[2];
+
+        if (c >= a + b) {
+            return "Invalid";
+        }
+        
+        if (a == b && b == c && c == a) {
+            return "Equilateral";
+        }
+
+        if (a == b || b == c || c == a) {
+            return "Isosceles";
+        }
+
+        return "Scalene";
+    }
 }
