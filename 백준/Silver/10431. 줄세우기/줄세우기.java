4f237cf7c47c6137be5c707125e693da074296b0
Sohyeon Kim <thgus345@naver.com>
2024-09-27 15:18:48 +0900

    [Silver V] Title: 줄세우기, Time: 204 ms, Memory: 19200 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,40 +1,55 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    public static void main(String[] args) throws Exception {
+        int T = Integer.valueOf(br.readLine());
 
-		int T = Integer.parseInt(br.readLine());
-		while (T-- > 0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int P = Integer.parseInt(st.nextToken());
-			int cnt = 0;
-			List<Integer> al = new ArrayList<>();
-			for (int i = 0; i < 20; i++) {
-				int h = Integer.parseInt(st.nextToken());
-				if (al.isEmpty()) {
-					al.add(h);
-				} else {
-					if (h > al.get(al.size() - 1)) {
-						al.add(h);
-						continue;
-					} else if (h < al.get(0)) {
-						al.add(0, h);
-						cnt += al.size() - 1;
-						continue;
-					}
-					for (int j = al.size() - 2; j >= 0; j--) {
-						if (h > al.get(j)) {
-							al.add(j + 1, h);
-							cnt += al.size() - j - 2;
-							break;
-						}
-					}
-				}
-			}
-			System.out.println(P + " " + cnt);
-		}
-	}
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.valueOf(st.nextToken());
+            int[] arr = new int[20];
+            for (int j = 0; j < 20; j++) {
+                arr[j] = Integer.valueOf(st.nextToken());
+            }
+            bw.write(String.valueOf(num) + " " + String.valueOf(solve(arr)) + "\n");
+        }
 
+        bw.flush();
+    }
+
+    private static int solve(int[] arr) {
+        int sum = 0;
+        List<Integer> al = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            int h = arr[i];
+
+            if (al.isEmpty()) {
+                al.add(h);
+            } else {
+                if (h > al.get(al.size() - 1)) {
+                    al.add(h);
+                } else if (h < al.get(0)) {
+                    al.add(0, h);
+                    sum += al.size() - 1;
+                } else {
+                    for (int j = al.size() - 2; j >= 0; j--) {
+                        if (h > al.get(j)) {
+                            al.add(j + 1, h);
+                            sum += al.size() - j - 2;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return sum;
+    }
 }
