2b36b49dd6270a5452d6674ab702827c75b7b36c
Sohyeon Kim <thgus345@naver.com>
2024-09-28 01:41:42 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,48 +1,68 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-		int[] switches = new int[N];
+    public static void main(String[] args) throws Exception {
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.valueOf(br.readLine());
+        int[] arr = new int[N + 1];
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			switches[i] = Integer.parseInt(st.nextToken());
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.valueOf(st.nextToken());
+        }
 
-		int M = Integer.parseInt(br.readLine());
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int gen = Integer.parseInt(st.nextToken());
-			int num = Integer.parseInt(st.nextToken());
-			if (gen == 1) { // 남학생
-				for (int j = num; j <= N; j += num) {
-					switches[j - 1] = switches[j - 1] == 0 ? 1 : 0;
-				}
-			} else if (gen == 2) { // 여학생
-				int j = 1;
-				switches[num - 1] = switches[num - 1] == 0 ? 1 : 0;
-				while (num + j <= N && num - j > 0) {
-					if (switches[num + j - 1] != switches[num - j - 1]) {
-						break;
-					}
-					switches[num + j - 1] = switches[num + j - 1] == 0 ? 1 : 0;
-					switches[num - j - 1] = switches[num - j - 1] == 0 ? 1 : 0;
-					j++;
-				}
-			}
-		}
+        int K = Integer.valueOf(br.readLine());
 
-		for (int i = 0; i < switches.length; i++) {
-			System.out.print(switches[i] + " ");
-			if ((i + 1) % 20 == 0) {
-				System.out.println();
-			}
-		}
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int gender = Integer.valueOf(st.nextToken());
+            int num = Integer.valueOf(st.nextToken());
 
-	}
+            if (gender == 1) {
+                boy(arr, num);
+            } else if (gender == 2) {
+                girl(arr, num);
+            }
+
+            //System.out.println(Arrays.toString(arr));
+        }
+
+        for (int i = 1; i <= N; i++) {
+            bw.write(String.valueOf(arr[i]) + " ");
+            if (i % 20 == 0) bw.write("\n");
+        }
+
+        bw.flush();
+    }
+
+    private static void boy(int[] arr, int num) {
+        for (int i = num; i < arr.length; i += num) {
+            arr[i] = arr[i] == 0 ? 1 : 0;
+        }
+    }
+
+    private static void girl(int[] arr, int num) {
+        int left = num - 1;
+        int right = num + 1;
+
+        while (0 < left && left < arr.length && 0 < right && right < arr.length) {
+            if (arr[left] != arr[right]) break;
+
+            arr[left] = arr[left] == 0 ? 1 : 0;
+            arr[right] = arr[left];
+
+            left--;
+            right++;
+        }
+
+        arr[num] = arr[num] == 0 ? 1 : 0;
+    }
+
 
 }
