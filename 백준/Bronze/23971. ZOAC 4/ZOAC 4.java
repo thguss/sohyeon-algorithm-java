c0facd6c91e00ad68d288a07db2da8628122d418
Sohyeon Kim <thgus345@naver.com>
2024-09-27 12:57:14 +0900

    [Bronze III] Title: ZOAC 4, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -1,30 +1,24 @@
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-import org.w3c.dom.Node;
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int H = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-public class Main {
+        int height = (H - 1) / (N + 1) + 1;
+        int weight = (W - 1) / (M + 1) + 1;
+        int ans = height * weight;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int H = Integer.parseInt(st.nextToken());
-		int W = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
-		int height = getPosCnt(H, N + 1);
-		int weight = getPosCnt(W, M + 1);
-
-		System.out.println(height * weight);
-	}
-
-	private static int getPosCnt(int maxLen, int posLen) {
-		int res = maxLen / posLen;
-		return (maxLen % posLen == 0) ? res : res + 1;
-	}
+        bw.write(String.valueOf(ans));
+        bw.flush();
+    }
 }
