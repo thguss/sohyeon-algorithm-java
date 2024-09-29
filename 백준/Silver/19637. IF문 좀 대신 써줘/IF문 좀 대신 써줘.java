518423e8f7c514ffb11103966a1085d4e068a2df
Sohyeon Kim <thgus345@naver.com>
2024-09-30 00:37:59 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 540 ms, Memory: 64304 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -1,54 +1,59 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
-	static int N, M;
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws Exception {
-		input();
-	}
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static void input() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.valueOf(st.nextToken());
+        int M = Integer.valueOf(st.nextToken());
 
-		N = Integer.parseInt(st.nextToken()); // 칭호 개수
-		M = Integer.parseInt(st.nextToken()); // 캐릭터 수
+        List<Integer> al = new ArrayList<>();
+        Map<Integer, String> map = new HashMap<>();
 
-		String[][] tag = new String[N][2];
-		StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String keyword = st.nextToken();
+            int limit = Integer.valueOf(st.nextToken());
+            if (!map.containsKey(limit)) {
+                al.add(limit);
+                map.put(limit, keyword);
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			tag[i][0] = st.nextToken(); // 칭호
-			tag[i][1] = st.nextToken(); // 전투력
-		}
+        Collections.sort(al);
 
-		for (int i = 0; i < M; i++) {
-			int hp = Integer.parseInt(br.readLine());
-			sb.append(bs(tag, hp)).append("\n");
-		}
+        for (int i = 0; i < M; i++) {
+            int score = Integer.valueOf(br.readLine());
+            bw.write(bs(al, map, score) + "\n");
+        }
+        
+        bw.flush();
+    }
 
-		System.out.println(sb);
+    private static String bs(List<Integer> keys, Map<Integer, String> map, int score) {
+        int left = 0;
+        int right = keys.size() - 1;
+        int min = Integer.MAX_VALUE;
 
-	}
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int key = keys.get(mid);
 
-	static String bs(String[][] tag, int hp) {
-		int left = 0;
-		int right = N - 1;
+            if (score <= key) {
+                right = mid - 1;
+                min = Math.min(min, key);
+            } else {
+                left = mid + 1;
+            }
+        }
 
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			int power = Integer.parseInt(tag[mid][1]);
-
-			if (power < hp) {
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-
-		return tag[left][0];
-	}
+        return map.get(min);
+    }
 
 }
