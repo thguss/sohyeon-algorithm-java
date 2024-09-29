9b0f32890094c0480b5f425179753f75766a597d
Sohyeon Kim <thgus345@naver.com>
2024-09-29 14:56:54 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 53424 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.valueOf(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            int next = queue.poll();
+            queue.add(next);
+        }
+
+        bw.write(String.valueOf(queue.poll()));
+
+        bw.flush();
+    }
+
+}
