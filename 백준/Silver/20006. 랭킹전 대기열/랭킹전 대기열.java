cd16a0076e114c77f35cb3e2a589b5a00aea4176
Sohyeon Kim <thgus345@naver.com>
2024-09-30 01:22:06 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 136 ms, Memory: 16508 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
@@ -1,69 +1,73 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+// The main method must be in a class named "Main".
+class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    
-    static class Room {
-        List<Player> players = new ArrayList<>();
-    }
-    
-    static class Player implements Comparable<Player> {
+
+    public static class Room {
         int level;
-        String nickname;
-        
-        public Player(int level, String nickname) {
+        List<User> users = new ArrayList<>();
+
+        Room(int level) {
             this.level = level;
-            this.nickname = nickname;
-        }
-        
-        @Override
-        public int compareTo(Player p) {
-            return this.nickname.compareTo(p.nickname);
         }
     }
 
-	public static void main(String[] args) throws Exception {
-	    StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int p = Integer.parseInt(st.nextToken()); // 플레이어 수
-		int m = Integer.parseInt(st.nextToken()); // 방의 정원
-		List<Room> rooms = new ArrayList<>();
+    public static class User implements Comparable<User> {
+        int level;
+        String id;
 
-		for (int i = 0; i < p; i++) {
-		    st = new StringTokenizer(br.readLine());
-		    int l = Integer.parseInt(st.nextToken()); // 레벨
-		    String n = st.nextToken(); // 닉네임
-		    boolean isEntered = false;
-		    
-		    for (Room room : rooms) {
-		        if (room.players.size() >= m) {
-		            continue;
-		        }
-		        if (!room.players.isEmpty() && Math.abs(l - room.players.get(0).level) <= 10) {
-		            isEntered = true;
-		            room.players.add(new Player(l, n));
-		            break;
-		        }
-		    }
-		    
-		    if (!isEntered) {
-		        Room room = new Room();
-		        room.players.add(new Player(l, n));
-		        rooms.add(room);
-		    }
-		}
-		
-		for (Room room : rooms) {
-		    Collections.sort(room.players);
-		    bw.write(room.players.size() == m ? "Started!\n" : "Waiting!\n");
-		    
-		    for (Player player : room.players) {
-		        bw.write(Integer.toString(player.level) + " " + player.nickname + "\n");
-		    }
-		}
-		
-		bw.flush();
-	}
+        public User(int level, String id) {
+            this.level = level;
+            this.id = id;
+        }
+
+        @Override
+        public int compareTo(User o) {
+            return this.id.compareTo(o.id);
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int p = Integer.valueOf(st.nextToken());
+        int m = Integer.valueOf(st.nextToken());
+        List<Room> rooms = new ArrayList<>();
+
+        for (int i = 0; i < p; i++) {
+            st = new StringTokenizer(br.readLine());
+            int level = Integer.valueOf(st.nextToken());
+            String id = st.nextToken();
+            User user = new User(level, id);
+            boolean isJoin = false;
+
+            for (Room room : rooms) {
+                if (room.users.size() == m || Math.abs(room.level - user.level) > 10) continue;
+                room.users.add(user);
+                isJoin = true;
+                break;
+            }
+
+            if (!isJoin) {
+                Room room = new Room(user.level);
+                room.users.add(user);
+                rooms.add(room);
+            }
+        }
+
+        for (Room room : rooms) {
+            boolean isStarted = room.users.size() == m;
+            bw.write(isStarted ? "Started!\n" : "Waiting!\n");
+            Collections.sort(room.users);
+            for (User user : room.users) {
+                bw.write(String.valueOf(user.level) + " " + user.id + "\n");
+            }
+        }
+
+        bw.flush();
+    }
+
 }
