6f8716d187ace0f128e2870b7bba84915a39b78a
Sohyeon Kim <thgus345@naver.com>
2024-09-30 00:07:04 +0900

    [Silver II] Title: KCPC, Time: 332 ms, Memory: 38872 KB -BaekjoonHub

--- 백준/Silver/3758. KCPC/KCPC.java
+++ 백준/Silver/3758. KCPC/KCPC.java
@@ -1,86 +1,114 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static class Team implements Comparable<Team> {
-		int id;
-		int score;
-		int count;
-		int lastAt;
+    static class Team implements Comparable<Team> {
+        int id;
+        int score;
+        int[] scores;
+        int count = 0;
+        int updatedAt = Integer.MAX_VALUE;
 
-		Team(int id, int score, int count, int lastAt) {
-			this.id = id;
-			this.score = score;
-			this.count = count;
-			this.lastAt = lastAt;
-		}
+        Team(int id, int k) {
+            this.id = id;
+            this.scores = new int[k + 1];
+        }
 
-		@Override
-		public int compareTo(Team o) {
-			if (o.score == this.score) {
-				if (this.count == o.count) {
-					return this.lastAt - o.lastAt;
-				}
-				return this.count - o.count;
-			}
-			return o.score - this.score;
-		}
-	}
+        public void updatedScore() {
+            int sum = 0;
+            for (int s : scores) {
+                sum += s;
+            }
+            this.score = sum;
+        }
 
-	public static void main(String[] args) throws Exception {
-		solve();
-	}
+        @Override
+        public int compareTo(Team o) {
+            if (this.score == o.score) {
+                if (this.count == o.count) {
+                    return this.updatedAt - o.updatedAt;
+                }
+                return this.count - o.count;
+            }
+            return o.score - this.score;
+        }
+    }
 
-	static void solve() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// StringTokenizer st = new StringTokenizer(br.readLine());
+    static class Log {
+        int i; // 팀
+        int j; // 문제 ID
+        int s; // 획득한 점수
+        int order;
 
-		int T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
+        Log(int i, int j, int s, int order) {
+            this.i = i;
+            this.j = j;
+            this.s = s;
+            this.order = order;
+        }
+    }
 
-		for (int a = 0; a < T; a++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken()); // 팀 개수
-			int k = Integer.parseInt(st.nextToken()); // 문제 개수
-			int t = Integer.parseInt(st.nextToken()); // 팀 ID
-			int m = Integer.parseInt(st.nextToken()); // 로그 엔트리 개수
+    public static void main(String[] args) throws Exception {
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.valueOf(br.readLine());
 
-			int[][] logs = new int[n + 1][k + 1];
-			Team[] teams = new Team[n + 1];
-			for (int i = 0; i <= n; i++) teams[i] = new Team(i, 0, 0, 0);
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.valueOf(st.nextToken()); // 팀의 개수
+            int k = Integer.valueOf(st.nextToken()); // 문제 개수
+            int t = Integer.valueOf(st.nextToken()); // 속한 팀의 ID
+            int m = Integer.valueOf(st.nextToken()); // 로그 엔트리 개수
 
-			for (int b = 0; b < m; b++) {
-				st = new StringTokenizer(br.readLine());
-				int i = Integer.parseInt(st.nextToken()); // 팀 ID
-				int j = Integer.parseInt(st.nextToken()); // 문제 번호
-				int s = Integer.parseInt(st.nextToken()); // 획득 점수
+            List<Log> logs = new ArrayList<>();
+            
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int teamId = Integer.valueOf(st.nextToken());
+                int solveId = Integer.valueOf(st.nextToken());
+                int score = Integer.valueOf(st.nextToken());
+                
+                logs.add(new Log(teamId, solveId, score, i));
+            }
 
-				logs[i][j] = Math.max(logs[i][j], s);
-				teams[i].count++;
-				teams[i].lastAt = b;
-			}
+            bw.write(String.valueOf(solve(n, k, t, logs)) + "\n");
+        }
+        
+        bw.flush();
+    }
 
-			for (int i = 1; i <= n; i++) {
-				for (int j = 1; j <= k; j++) {
-					teams[i].score += logs[i][j];
-				}
-			}
+    private static int solve(int n, int k, int t, List<Log> logs) {
+        Map<Integer, Team> map = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            map.put(i, new Team(i, k));
+        }
 
-			Arrays.sort(teams);
+        for (int i = 0; i < logs.size(); i++) {
+            Log log = logs.get(i);
+            Team team = map.get(log.i);
+            team.scores[log.j] = Math.max(team.scores[log.j], log.s);
+            team.count++;
+            team.updatedAt = i;
+        }
 
-			sb.append(getRank(teams, t)).append("\n");
-		}
+        List<Team> teams = new ArrayList<>();
+        for (int key : map.keySet()) {
+            Team team = map.get(key);
+            team.updatedScore();
+            teams.add(team);
+        }
 
-		System.out.println(sb);
+        Collections.sort(teams);
 
-	}
+        for (int i = 0; i < teams.size(); i++) {
+            if (teams.get(i).id == t) return i + 1;
+        }
 
-	static int getRank(Team[] teams, int t) {
-		for (int i = 0; i < teams.length; i++) {
-			if (teams[i].id == t) return i + 1;
-		}
-		return -1;
-	}
+        return teams.size();
+    }
 
 }
