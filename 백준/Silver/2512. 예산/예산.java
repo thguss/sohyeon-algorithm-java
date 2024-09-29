53721b5791c7de18f8ce73d693d8124f20bdfd81
Sohyeon Kim <thgus345@naver.com>
2024-09-29 16:18:32 +0900

    [Silver II] Title: 예산, Time: 152 ms, Memory: 15980 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,56 +1,47 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static int N, M, SUM;
+    public static void main(String[] args) throws Exception {
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.valueOf(br.readLine());
+        int[] arr = new int[N];
 
-	public static void main(String[] args) throws Exception {
-		int[] arr = getArr();
-		System.out.println(solve(arr));
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.valueOf(st.nextToken());
+        }
+        Arrays.sort(arr);
+        
+        int M = Integer.valueOf(br.readLine());
 
-	static int solve(int[] arr) {
-		Arrays.sort(arr);
+        int left = 0;
+        int right = M;
+        int max = 0;
 
-		if (M >= SUM) return arr[N - 1];
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int sum = 0;
 
-		int left = 0;
-		int right = arr[N - 1];
+            for (int i = 0; i < N; i++) {
+                sum += arr[i] <= mid ? arr[i] : mid;
+            }
 
-		while (left <= right) {
-			int mid = (left + right) / 2;
+            if (sum <= M) {
+                left = mid + 1;
+                max = Math.max(max, arr[N - 1] <= mid ? arr[N - 1] : mid);
+            } else {
+                right = mid - 1;
+            }
+        }
 
-			int sum = 0; // 상한액으로 얻을 수 있는 합계
-			for (int i = 0; i < N; i++) {
-				sum += Math.min(arr[i], mid);
-			}
-
-			if (sum <= M) left = mid + 1;
-			else right = mid - 1;
-		}
-
-		return right;
-	}
-
-	static int[] getArr() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// StringTokenizer st = new StringTokenizer(br.readLine());
-
-		N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		SUM = 0;
-		int[] arr = new int[N];
-
-		for (int i = 0; i < N; i++) {
-			int cost = Integer.parseInt(st.nextToken());
-			SUM += cost;
-			arr[i] = cost;
-		}
-
-		M = Integer.parseInt(br.readLine());
-
-		return arr;
-	}
+        bw.write(String.valueOf(max));
+        bw.flush();
+    }
 
 }
