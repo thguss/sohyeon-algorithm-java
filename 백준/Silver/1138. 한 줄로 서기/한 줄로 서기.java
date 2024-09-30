aab53c75d0d980d869a3181da1e28b82320f06bd
Sohyeon Kim <thgus345@naver.com>
2024-09-30 10:20:51 +0900

    [Silver II] Title: 한 줄로 서기, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,33 +1,37 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+    public static void main(String[] args) throws Exception {
+        int N = Integer.valueOf(br.readLine());
+        int[] arr = new int[N];
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.valueOf(st.nextToken());
+        }
 
-		List<Integer> al = new ArrayList<>();
+        List<Integer> al = new ArrayList<>();
+        for (int i = N - 1; i >= 0; i--) {
+            int cnt = arr[i];
 
-		for (int i = N - 1; i >= 0; i--) { // 키 큰 사람부터
-			int cnt = arr[i];
-			if (cnt < al.size()) {
-				al.add(cnt, i + 1);
-			} else {
-				al.add(i + 1);
-			}
-		}
+            if (cnt < al.size()) {
+                al.add(cnt, i + 1);
+            } else {
+                al.add(i + 1);
+            }
+        }
 
-		for (int num : al) {
-			System.out.print(num + " ");
-		}
+        for (int i : al) {
+            bw.write(String.valueOf(i) + " ");
+        }
 
-	}
+        bw.flush();
+    }
 
 }
