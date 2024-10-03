e34ed1085a59d0216f8e8e44416726d1fa8429ff
Sohyeon Kim <thgus345@naver.com>
2024-10-03 21:37:16 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 116 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -1,49 +1,35 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws Exception {
-		solve();
-	}
+    public static void main(String[] args) throws Exception {
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.valueOf(br.readLine());
 
-	static void solve() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// StringTokenizer st = new StringTokenizer(br.readLine());
+        int[][] dp = new int[10001][4];
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+        for (int i = 4; i <= 10000; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        }
 
-		int T = Integer.parseInt(br.readLine());
-		int[][] dp = dp();
-		StringBuilder sb = new StringBuilder();
-
-		while (T-- > 0) {
-			int n = Integer.parseInt(br.readLine());
-			sb.append(dp[n][1] + dp[n][2] + dp[n][3]).append("\n");
-		}
-
-		System.out.println(sb);
-
-	}
-
-	static int[][] dp() {
-		int[][] dp = new int[10001][4];
-
-		dp[1][1] = 1; // 1
-
-		dp[2][1] = 1; // 1 + 1
-		dp[2][2] = 1; // 2
-
-		dp[3][1] = 1; // 1 + 1 + 1
-		dp[3][2] = 1; // 1 + 2
-		dp[3][3] = 1; // 3
-
-
-		for (int i = 4; i < 10001; i++) {
-			dp[i][1] = dp[i - 1][1];
-			dp[i][2] = dp[i - 2][2] + dp[i - 2][1];
-			dp[i][3] = dp[i - 3][3] + dp[i - 3][2] + dp[i - 3][1];
-		}
-
-		return dp;
-	}
+        while (T-- > 0) {
+            int num = Integer.valueOf(br.readLine());
+            bw.write(String.valueOf(dp[num][1] + dp[num][2] + dp[num][3]) + "\n");
+        }
+        
+        bw.flush();
+    }
 
 }
