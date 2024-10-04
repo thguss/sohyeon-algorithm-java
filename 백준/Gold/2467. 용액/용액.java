385dc29cf07ed11d92fa032a5f0daee25c91ada3
Sohyeon Kim <thgus345@naver.com>
2024-10-04 19:22:28 +0900

    [Gold V] Title: 용액, Time: 344 ms, Memory: 33888 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,35 +1,53 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+// The main method must be in a class named "Main".
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+    private static int[] solve(long[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        long min = Long.MAX_VALUE;
+        int[] result = new int[2];
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long[] arr = new long[N + 1];
-		for (int i = 0; i < N; i++) {
-			arr[i] = Long.parseLong(st.nextToken());
-		}
+        while (left < right) {
+            long sum = arr[left] + arr[right];
 
-		int left = 0, right = N - 1;
-		int l = 0, r = 0;
-		long min = Long.MAX_VALUE;
-		while (left < right) {
-			long sum = arr[left] + arr[right];
-			if (min > Math.abs(sum)) {
-				min = Math.abs(sum);
-				l = left;
-				r = right;
-			}
-			if (sum >= 0) {
-				right--;
-			} else {
-				left++;
-			}
-		}
+            if (min > Math.abs(sum)) {
+                min = Math.abs(sum);
+                result = new int[] {left, right};
+            }
 
-		System.out.println(arr[l] + " " + arr[r]);
-	}
+            if (sum > 0) {
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                return new int[] {left, right};
+            }
+            
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.valueOf(br.readLine());
+        long[] arr = new long[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.valueOf(st.nextToken());
+        }
+
+        int[] res = solve(arr);
+
+        bw.write(String.valueOf(arr[res[0]]) + " " + String.valueOf(arr[res[1]]));
+        bw.flush();
+    }
+
 }
